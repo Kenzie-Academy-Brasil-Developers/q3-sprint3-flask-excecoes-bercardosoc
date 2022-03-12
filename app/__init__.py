664d585674d9ef8ec4a88cc51b5abfd0d0010583
Bernardo <bernardocardosodacosta@gmail.com>
2022-03-12 16:06:43 -0300

    feat: create again get and post functions and delete old bugging files treating classes

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,77 @@
-from http import HTTPStatus
-from flask import Flask, jsonify, request
-from app.classes.user_class import User
-app = Flask(__name__)
 
-DATABASE_FILEPATH = "database/database.json"
+import os
+from http import HTTPStatus
+from ujson import load, dump
+from flask import Flask, request
+
+app = Flask(__name__)
 
 @app.get("/user")
 def get_users():
-    return jsonify(User.get_users()), HTTPStatus.OK
+    try:
+        os.mkdir("app/database")
+        os.system("touch app/database/database.json")
+        with open(f"./app/database/database.json", "a") as json_file:
+            json_file.write('{"data": []}')
+        with open(f"./app/database/database.json", "r") as json_file:
+            readable_file = json_file.read()
+        return readable_file, HTTPStatus.OK
+    
+    except FileExistsError:
+        with open(f"./app/database/database.json", "r") as json_file:
+            readable_file = json_file.read()
+        if readable_file == "":
+            with open(f"./app/database/database.json", "a") as json_file:
+                json_file.write('{"data": []}')
+            with open(f"./app/database/database.json", "r") as json_file:
+                readable_file = json_file.read()
+                return readable_file, HTTPStatus.OK
+        else: 
+            with open(f"./app/database/database.json", "r") as json_file:
+                readable_file = load(json_file)
+                return readable_file, HTTPStatus.OK
 
 @app.post("/user")
-def creating_user():
+def post_user():
 
-    # data = request.get_json()
-    # user = User(**data)     
-    new_user = User(**request.get_json())
-    return new_user.create_user(), HTTPStatus.CREATED
+    users = ""
+
+    try:
+        with open("./app/database/database.json", "r") as json_file:
+            users = load(json_file)
+    except FileNotFoundError:
+        os.mkdir("app/database")
+        os.system("touch app/database/database.json")
+        with open(f"./app/database/database.json", "a") as json_file:
+            json_file.write("{data: []}")
+        with open ("./app/database/database.json", "r") as json_file:
+            users = load(json_file)
+
+    if type(request.get_json()["email"]) != str and type(request.get_json()["name"]) != str:
+        return {"message": "Wrong fields format"}, HTTPStatus.BAD_REQUEST
+    elif type(request.get_json()["email"]) != str:
+        return {"message": "Wrong email field format"}, HTTPStatus.BAD_REQUEST
+    elif type(request.get_json()["name"]) != str:
+        return {"message": "Wrong name field format"}, HTTPStatus.BAD_REQUEST
+
+    input_email = request.get_json()["email"]
+    input_name  = request.get_json()["name"]
+
+    email = input_email.lower()
+    name = input_name.title()
+
+    user_data = {
+        "email": f"{email}",
+        "name": f"{name}"
+    }
+
+    for data in users["data"]:
+        if data["email"] == email:
+            return {"message": "E-mail already exists"}, HTTPStatus.CONFLICT
+    with open("./app/database/database.json", "w") as json_file:
+        users["data"].append(user_data)
+        dump(users, json_file, indent=2)
+        return {
+            "data": user_data
+        }, HTTPStatus.CREATED
     
