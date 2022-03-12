e368409f4544edaf393e0eda9c75aae8eb0679ce
Bernardo <bernardocardosodacosta@gmail.com>
2022-03-12 10:27:26 -0300

    feat: create a function that returns a database creation or an exception

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,30 @@
-from flask import Flask
 
+import os
+from flask import Flask
+from ujson import dump
 
 app = Flask(__name__)
+
+def create_file():
+    os.chdir("./database")
+    default = {"data": []}
+    with open("database.json", "w") as json_file:
+        dump(default, json_file, indent=2)
+
+def check_database_dir():
+    directories = os.listdir()
+    parent_dir = os.getcwd()
+    directory = "database"
+    path = os.path.join(parent_dir, directory)
+    for item in directories:
+        if item != directory:
+            os.mkdir(path)
+            create_file()
+
+@app.get("/user")
+def get_users():
+    try:
+        check_database_dir()
+    except FileExistsError:
+        return "User"
+        # Retornar o que achamos 
